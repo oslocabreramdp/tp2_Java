7636f0d4aa290af3e3ab1eb14ec61a1cd86c0b41
oslocabreramdp <oslo.cabrera.mdp@gmail.com>
2026-09-03 15:27:51 -0300

    Avance consigna 3

--- src/main/java/Cliente.java
+++ src/main/java/Cliente.java
@@ -1,2 +1,84 @@
+import java.util.Objects;
+import java.util.UUID;
+
 public class Cliente {
+
+    public static int BASE =0;
+    public static int TOPE =15;
+
+
+    private String id_cliente;
+    private String nombre;
+    private String email;
+    private double descuento;
+
+    public Cliente(String nombre, String email, double descuento) {
+        this.id_cliente=UUID.randomUUID().toString().substring(BASE, TOPE)
+        this.nombre = nombre;
+        this.email = email;
+        this.descuento = descuento;
+    }
+
+    public Cliente() {
+        this.id_cliente=UUID.randomUUID().toString().substring(BASE, TOPE);
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDescuento(double descuento) {
+        this.descuento = descuento;
+    }
+
+    public static int getBASE() {
+        return BASE;
+    }
+
+    public static int getTOPE() {
+        return TOPE;
+    }
+
+    public String getId_cliente() {
+        return id_cliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public double getDescuento() {
+        return descuento;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id_cliente='" + id_cliente + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", email='" + email + '\'' +
+                ", descuento=" + descuento +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Cliente cliente)) return false;
+        return Objects.equals(id_cliente, cliente.id_cliente);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id_cliente);
+    }
+
+
 }
--- src/main/java/Factura.java
+++ src/main/java/Factura.java
@@ -1,2 +1,80 @@
-public class Factura extends Cliente {
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+import java.util.UUID;
+import java.time.LocalDateTime
+
+public class Factura {
+
+    LocalDateTime fechaActual = LocalDateTime.now();
+    DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy --- HH:mm:ss");
+
+    private String id_factura;
+    private Cliente realiza_compra;
+    private String fecha;
+    private double  monto_compra;
+
+    public Factura(Cliente realiza_compra, double monto_compra) {
+        this.id_factura=UUID.randomUUID().toString().substring(realiza_compra.getBASE(),realiza_compra.getTOPE());
+        this.fecha = fechaActual.format(formato);
+        this.realiza_compra = realiza_compra;
+        this.monto_compra = monto_compra;
+    }
+
+    public Factura() {
+        this.id_factura=UUID.randomUUID().toString().substring(realiza_compra.getBASE(),realiza_compra.getTOPE());
+        this.fecha = fechaActual.format(formato);
+    }
+
+    public void setRealiza_compra(Cliente realiza_compra) {
+        this.realiza_compra = realiza_compra;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setMonto_compra(double monto_compra) {
+        this.monto_compra = monto_compra;
+    }
+
+    public String getId_factura() {
+        return id_factura;
+    }
+
+    public Cliente getRealiza_compra() {
+        return realiza_compra;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public double getMonto_compra() {
+        return monto_compra;
+    }
+
+    @Override
+    public String toString() {
+        return "Factura{" +
+                "id_factura='" + id_factura + '\'' +
+                ", realiza_compra=" + realiza_compra +
+                ", fecha='" + fecha + '\'' +
+                ", monto_compra=" + monto_compra +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Factura factura)) return false;
+        return Objects.equals(id_factura, factura.id_factura) && Objects.equals(realiza_compra, factura.realiza_compra);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id_factura, realiza_compra);
+    }
+
+    public double monto_a_cobrar(){
+        return monto_compra*(1-(realiza_compra.getDescuento()/100));
+    }
 }
